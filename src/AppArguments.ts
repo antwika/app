c6e4b8a247b1e7300ce9ef7d81771fb47e672c2b
Anton Wiklund <ixuz07@gmail.com>
2022-05-04 17:58:11 +0200

    fix: a temporary fix for TS7053 related yargs.argv being sync/async.

--- src/AppArguments.ts
+++ src/AppArguments.ts
@@ -15,9 +15,17 @@ export class AppArguments {
       type: 'string',
     }));
 
+    // Appears that the y.argv may 'sometimes' be a Promise, it's unclear
+    // currently when this is might be, and if it's a problem for the application.
+    // See issue on this topic: https://github.com/yargs/yargs/issues/2175
+    // TODO: Address issue accordingly to response, e.g.:
+    // 1. Utilize yargs parsing in a synchronous way (if possible)
+    // 2. Or move the yargs parsing via an async getter function
+    const argv = y.argv as any;
+
     this.args = this.args.map((arg) => ({
       ...arg,
-      value: y.argv[arg.shortName] as string,
+      value: argv[arg.shortName] as string,
     }));
   }
 }
