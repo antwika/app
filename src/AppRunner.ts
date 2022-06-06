7e986ff5e028026ba62302b790f693c8773a6087
Anton Wiklund <ixuz07@gmail.com>
2022-06-06 15:46:57 +0200

    feat: inject environment variables starting with 'APP_' into application

--- src/AppRunner.ts
+++ src/AppRunner.ts
@@ -7,9 +7,15 @@ export class AppRunner {
       mainModulePath: process.cwd(),
     });
 
+    const keys = Object.keys(process.env).filter((key) => key.startsWith('APP_'));
+    const variables = keys.reduce((acc, key) => {
+      acc[`urn:antwika:app:args:${key}`] = process.env[key] as string;
+      return acc;
+    }, {} as Record<string, string>);
+
     await manager.configRegistry.register('config/config.jsonld');
 
-    const myApp: App = await manager.instantiate('urn:antwika:app:App');
+    const myApp: App = await manager.instantiate('urn:antwika:app:App', { variables });
     await myApp.start();
   }
 }
--- test/AppRunner.test.ts
+++ test/AppRunner.test.ts
@@ -16,11 +16,31 @@ jest.mock('componentsjs', () => ({
 }));
 
 describe('AppRunner', () => {
+  const originalProcessEnv = process.env;
+
+  beforeEach(() => {
+    process.env = { ...originalProcessEnv };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    process.env = originalProcessEnv;
+  });
+
   it('can be started and stopped', async () => {
     const appRunner = new AppRunner();
     await appRunner.run(process);
     expect(configRegistry.register).toHaveBeenCalledWith('config/config.jsonld');
-    expect(instantiate).toHaveBeenCalledWith('urn:antwika:app:App');
+    expect(instantiate).toHaveBeenCalledWith('urn:antwika:app:App', { variables: {} });
+    expect(app.start).toHaveBeenCalledTimes(1);
+  });
+
+  it('can be started with environment variables', async () => {
+    const appRunner = new AppRunner();
+    process.env.APP_VARIABLE = '123';
+    await appRunner.run(process);
+    expect(configRegistry.register).toHaveBeenCalledWith('config/config.jsonld');
+    expect(instantiate).toHaveBeenCalledWith('urn:antwika:app:App', { variables: { 'urn:antwika:app:args:APP_VARIABLE': '123' } });
     expect(app.start).toHaveBeenCalledTimes(1);
   });
 });
