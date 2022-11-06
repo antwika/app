6f4d1cc537768995e429fcccbc48993246b837bf
Anton Wiklund <ixuz07@gmail.com>
2022-11-06 20:40:25 +0100

    fix: utilize yargs parseSync for cli parsing inside a constructor

--- src/AppArguments.ts
+++ src/AppArguments.ts
@@ -16,14 +16,7 @@ export class AppArguments {
       type: 'string',
     }));
 
-    // Appears that the y.argv may 'sometimes' be a Promise, it's unclear
-    // currently when this is might be, and if it's a problem for the application.
-    // See issue on this topic: https://github.com/yargs/yargs/issues/2175
-    // TODO: Address issue accordingly to response, e.g.:
-    // 1. Utilize yargs parsing in a synchronous way (if possible)
-    // 2. Or move the yargs parsing via an async getter function
-    const argv = y.argv as any;
-
+    const argv = y.parseSync();
     this.args = this.args.map((arg) => ({
       ...arg,
       value: argv[arg.shortName] as string,
